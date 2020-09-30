28e4c91c2e02c91d2cae8567775fcf61dfd82849
alandin1 <alandin1@ucol.mx>
2020-09-30 14:34:51 -0500

    1001 a 1200

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var sueldo = 777;
 
-if (sueldo > 0 && sueldo < 1000){
+if (sueldo >= 0 && sueldo <= 1000){
     sueldo = sueldo + (sueldo * .15)
 } 
-
+else if (sueldo >= 1001 && sueldo <= 1200){
+    sueldo = sueldo + (sueldo * .12)
+} 
 console.log('Sueldo final:', sueldo)
